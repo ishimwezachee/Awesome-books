cb51db0871b0b5905059ccf73a3a988618c38dec
ishimwezachee <zacheeishimwe@gmail.com>
2021-10-18 19:51:52 +0200

    fix linters

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-const main = document.querySelector("#main");
-const divbooks = document.querySelector(".books");
-const inputTitle = document.querySelector("#title");
-const inputAuthor = document.querySelector("#author");
-const addBtn = document.querySelector("#add");
+const divbooks = document.querySelector('.books');
+const inputTitle = document.querySelector('#title');
+const inputAuthor = document.querySelector('#author');
+const addBtn = document.querySelector('#add');
 
-
-let savedData = localStorage.getItem("savedInput");
+const savedData = localStorage.getItem('savedInput');
 
 let collection = [];
 
@@ -13,10 +11,10 @@ if (savedData && savedData !== null) {
   collection = JSON.parse(savedData);
 }
 
-const displayData =()=> {
-    divbooks.innerHTML = "";
-    collection.forEach((value, index) => {
-        divbooks.innerHTML += `
+const displayData = () => {
+  divbooks.innerHTML = '';
+  collection.forEach((value, index) => {
+    divbooks.innerHTML += `
             <div class="books">
             <ul>
                 <li class="title">${value.name}</li>
@@ -25,21 +23,20 @@ const displayData =()=> {
              <button onclick="removeBook(${index});">remove</button>
              <hr>
            </div>`;
-      });
-}
+  });
+};
 
- displayData();
- const saveData = () => localStorage.setItem("savedInput", JSON.stringify(collection));
- const removeBook = (index) =>{
+displayData();
+const saveData = () => localStorage.setItem('savedInput', JSON.stringify(collection));
+const removeBook = (index) => {
   collection.splice(index, 1);
   saveData();
   displayData();
-}
+};
 
-
-addBtn.addEventListener("click", (e) => {
+addBtn.addEventListener('click', (e) => {
   e.preventDefault();
-  let newData = {
+  const newData = {
     name: inputTitle.value,
     author: inputAuthor.value,
   };
