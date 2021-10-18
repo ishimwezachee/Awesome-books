47be509b33f76c4f02e2a69a512bea659acd9adb
ishimwezachee <zacheeishimwe@gmail.com>
2021-10-18 19:31:33 +0200

    Add the item to local storage

--- index.js
+++ index.js
@@ -1,27 +1,44 @@
-const main = document.querySelector('#main')
-const divbooks = document.querySelector('.books');
-const collection = [
-    {
-        name:'singleness',
-        author:'author'
-    },
-    {
-        name:'love',
-        author:'Mucyo Alex'
-    },
-]
+const main = document.querySelector("#main");
+const divbooks = document.querySelector(".books");
+const inputTitle = document.querySelector("#title");
+const inputAuthor = document.querySelector("#author");
+const addBtn = document.querySelector("#add");
 
 
-collection.forEach(value=>{
-    divbooks.innerHTML +=`
-    <div class="books">
-    <ul>
-        <li class="title">${value.name}</li>
-        <li class="author">${value.author}</li>
-    </ul>
-     <button>remove</button>
-     <hr>
-   </div>`
-   })
+let savedData = localStorage.getItem("savedInput");
+
+let collection = [];
+
+if (savedData && savedData !== null) {
+  collection = JSON.parse(savedData);
+}
+
+const displayData =()=> {
+    divbooks.innerHTML = "";
+    collection.forEach((value, index) => {
+        divbooks.innerHTML += `
+            <div class="books">
+            <ul>
+                <li class="title">${value.name}</li>
+                <li class="author">${value.author}</li>
+            </ul>
+             <button onclick="removeBook(${index});">remove</button>
+             <hr>
+           </div>`;
+      });
+}
+
+ displayData();
+ const saveData = () => localStorage.setItem("savedInput", JSON.stringify(collection));
 
 
+addBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  let newData = {
+    name: inputTitle.value,
+    author: inputAuthor.value,
+  };
+  collection.push(newData);
+  saveData();
+  displayData();
+});
